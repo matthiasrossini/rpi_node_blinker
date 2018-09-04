315b877e543fe90f5a4e5d06d3575cb4c99cfeb4
Bryan Giger <bryan@gigerbyt.es>
2018-09-04 21:25:37 +0000

    initial demo

---
+++ blink.js
@@ -0,0 +1,20 @@
+var Gpio = require('onoff').Gpio;
+var LED = new Gpio(4, 'out');
+// var blinkIntnerval = setInterval(blinkLED, 250);
+
+module.exports = {
+
+
+blinkLED : function() {
+	if(LED.readSync() === 0) {
+		LED.writeSync(1);
+	} else {
+		LED.writeSync(0)
+	}
+},
+
+endBlink : function() {
+	LED.writeSync(0);
+}
+}
+
---
+++ index.js
@@ -0,0 +1,29 @@
+var express = require("express")
+var blink = require("./blink")
+var onoff = require("onoff")
+var app = express()
+
+var Blinker = {
+	blinker : null,
+	Start : function(){
+		if(!this.blinker){
+			this.blinker = setInterval(function(){blink.blinkLED()},250);
+		}
+	},
+	Stop : function(){
+		clearInterval(this.blinker)
+		this.blinker= null
+		blink.endBlink()
+	}
+}
+
+app.get("/blink", function(req, res) {
+	Blinker.Start()
+	res.send("Light should be blinking");
+})
+
+app.get("/stop", function(req,res){
+	Blinker.Stop()
+	res.send("the Blinking should stop")
+})
+app.listen(3000, '0.0.0.0')
